40198c82ec06fd70b8e795a8094ecd6abef5cbb9
Kevin Geoffery O'Hearon <ohearonkg@Kevins-MacBook-Pro.local>
2017-12-11 10:08:39 -0400

    starting to test on click functions with sinon

--- __tests__/PhotoIndicator.test.tsx
+++ __tests__/PhotoIndicator.test.tsx
@@ -1,41 +1,45 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
 import { PhotoIndicator } from '../src/components/PhotoIndicator';
+import * as sinon from 'sinon';
 
 describe('Photo Indicator', () => {
 
     it('Should Take A Prop Representing The Currently Selected Photos Number And Render It', () => {
-        const wrapper = shallow(<PhotoIndicator currentPhotoNumber={1} totalNumberPhotos={7}/>); 
+        const wrapper = shallow(<PhotoIndicator nextArrowClickFunction={ () => { return } } currentPhotoNumber={1} totalNumberPhotos={7}/>); 
         expect(wrapper.html()).toMatch(new RegExp('1'))
     });
 
     it('Should Add A Zero In Front of The Number Representing The Currently Selected Photo Should It Be A Single Digit', () => {
-        const wrapper = shallow(<PhotoIndicator currentPhotoNumber={1} totalNumberPhotos={7}/>); 
+        const wrapper = shallow(<PhotoIndicator nextArrowClickFunction={ () => { return } } currentPhotoNumber={1} totalNumberPhotos={7}/>); 
         expect(wrapper.html()).toMatch(new RegExp('01'))
     });
 
     it('Should NOT Add A Zero In Front of The Number Representing The Currently Selected Photo Should It Be A Two Digit Number', () => {
-        const wrapper = shallow(<PhotoIndicator currentPhotoNumber={11} totalNumberPhotos={7}/>); 
+        const wrapper = shallow(<PhotoIndicator nextArrowClickFunction={ () => { return } } currentPhotoNumber={11} totalNumberPhotos={7}/>); 
         expect(wrapper.html()).not.toMatch(new RegExp('011'))
     });
 
     it('Should Take A Prop Representing The Total Number of Photos And Render It', () => {
-        const wrapper = shallow(<PhotoIndicator currentPhotoNumber={1} totalNumberPhotos={7}/>); 
+        const wrapper = shallow(<PhotoIndicator nextArrowClickFunction={ () => { return } } currentPhotoNumber={1} totalNumberPhotos={7}/>); 
         expect(wrapper.html()).toMatch(new RegExp('7'))
     });
 
     it('Should Add A Zero In Front of The Number Representing The Total Number of Photos Should It Be A Single Digit', () => {
-        const wrapper = shallow(<PhotoIndicator currentPhotoNumber={1} totalNumberPhotos={7}/>); 
+        const wrapper = shallow(<PhotoIndicator nextArrowClickFunction={ () => { return } } currentPhotoNumber={1} totalNumberPhotos={7}/>); 
         expect(wrapper.html()).toMatch(new RegExp('07'))
     });
 
     it('Should NOT Add A Zero In Front of The Number Representing The Total Number of Photos Should It Be A Two Digit Number', () => {
-        const wrapper = shallow(<PhotoIndicator currentPhotoNumber={1} totalNumberPhotos={77}/>); 
+        const wrapper = shallow(<PhotoIndicator nextArrowClickFunction={ () => { return } } currentPhotoNumber={1} totalNumberPhotos={77}/>); 
         expect(wrapper.html()).not.toMatch(new RegExp('077'))
     });
 
     it('Should Take As A Prop A Function Which Should Be Called When Clicking The Next Arrow', () => {
-        expect(false).toBeTruthy();
+        const nextArrowSampleFunction = sinon.spy();
+        const wrapper = shallow(<PhotoIndicator nextArrowClickFunction={nextArrowSampleFunction} currentPhotoNumber={1} totalNumberPhotos={77}/>); 
+        wrapper.find('.photo-indicator__next-arrow-container').simulate('click');
+        expect(nextArrowSampleFunction['callCount']).toEqual(1);
     });
 
     it('Should Take As A Prop A Function Which Should Be Called When Clicking The Previous Arrow', () => {
