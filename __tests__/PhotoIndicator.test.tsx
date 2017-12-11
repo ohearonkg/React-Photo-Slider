968eedb248e09066a5f11b1c2ee9f88405857fa2
Kevin-Geoffery O'Hearon <ohearonkg@gmail.com>
2017-12-10 23:44:58 -0400

    first failing test. Need to make photo indicator.

---
+++ __tests__/PhotoIndicator.test.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+import { PhotoIndicator } from '../src/components/PhotoIndicator';
+
+describe('Photo Indicator', () => {
+    it('Should Take A Prop Representing The Currently Selected Photos Number And Render It', () => {
+        const wrapper = shallow(<PhotoIndicator currentPhoto='1' totalPhotos='3'/>);
+        expect(wrapper.html()).toMatch(new RegExp('1'))
+    })
+})
