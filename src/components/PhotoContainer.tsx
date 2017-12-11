509746ce9e74c8563dd1e663a876609e58f3cc66
Kevin Geoffery O'Hearon <ohearonkg@Kevins-MacBook-Pro.local>
2017-12-11 10:42:53 -0400

    now rendering

---
+++ src/components/PhotoContainer.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export interface PhotoContainerProps {
+  
+}
+
+export const PhotoContainer = (props:PhotoContainerProps) => {
+  return (
+    <h1> HELLO </h1>
+  )
+}
