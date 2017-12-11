488c07e8c59da85638909784e2035769ef1f2441
Kevin Geoffery O'Hearon <ohearonkg@Kevins-MacBook-Pro.local>
2017-12-11 12:54:55 -0400

    removed unused gsap and gsap types.

--- src/components/PhotoContainer.tsx
+++ src/components/PhotoContainer.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
+require('../styles/_photo-container.scss');
 
 export interface PhotoContainerProps {
   photoUrl: string;
 }
 
 export class PhotoContainer extends React.Component<PhotoContainerProps, {}> {
+  private photoReference: HTMLImageElement;
 
   componentDidMount() {
-    console.log('Hello');
+    setTimeout( () => {
+      this.photoReference.style.clipPath = "polygon(0 0, 100% 0, 100% 100% ,0 100%)";
+      this.photoReference.style.opacity = '1';
+    }, 1000);
   }
 
   render() {
     return (
-      <img src={this.props.photoUrl} />
+      <div className='photo-container'>
+        <div className='photo-container__wrapper'>
+          <img className='photo-container__photo' ref={ photo => this.photoReference = photo } src={this.props.photoUrl} />
+        </div>
+      </div>
     )
   }
 }
